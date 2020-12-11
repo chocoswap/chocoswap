70240143695f37cd11a65bbd70a8ea4949d67472
ericswap <ericswap@protonmail.com>
2020-12-11 17:31:57 +0800

    update

--- contracts/Choco.sol
+++ contracts/Choco.sol
@@ -4,12 +4,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract ChocoToken is ERC20("Choco Swap", "Choco") {
-    // address constant public preSale = 0x2408c20608d819ebEE26653B272579dCb196C0e9;
-    // address constant  dev = 0xCf4c701f361BC94421F7734723f8Cb08baE63783;
-    // address constant  tradeMining = 0xE101110384203a9aBb3d4A9BcFCF1Fecc2e1E411;
-    constructor(address preSale, address dev, address tradeMining) public {
-        _mint(preSale, 1e24);
-        _mint(dev, 1e24);
-        _mint(tradeMining, 8e24);
+    constructor(address market, address dev, address team, address partner, address other) public {
+        _mint(market, 2100000 ether);
+        _mint(dev, 2100000 ether);
+        _mint(team, 2100000 ether);
+        _mint(partner, 630000 ether);
+        _mint(other, 14070000 ether);
     }
 }
